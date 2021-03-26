cb248237208f2b4b04fe19ca3e60f7e22dd31448
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2021-03-26 15:01:02 -0400

    Criando respositorio generico para base dos demais, e implementando a classe de contexto para conexão com banco.

---
+++ GestaoMais.Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestaoMais.Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(GetStrConn());
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        private string GetStrConn()
+        {
+            var dataSource = "DESKTOP\\DESENVOLVIMENTO";
+            var database = "creamt-gestao-biblioteca";
+            var user = "sa";
+            var password = "1234";
+
+            return $"Data Source={dataSource};Initial Catalog={database};Integrated Security=False;User ID={user};Password={password};Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+        }
+    }
+}
---
+++ GestaoMais.Infrastructure/Repository/Generics/RepositoryGeneric.cs
@@ -0,0 +1,84 @@
+﻿using GestaoMais.Domain.Interfaces.Generics;
+using GestaoMais.Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace GestaoMais.Infrastructure.Repository.Generics
+{
+    public class RepositoryGeneric<T> : IGeneric<T>, IDisposable where T : class
+    {
+        private readonly DbContextOptions<ContextBase> _optionsBuilder;
+
+        public RepositoryGeneric()
+        {
+            _optionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        private bool disposed = false;
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+                handle.Dispose();
+
+            disposed = true;
+        }
+
+        public async Task Add(T obj)
+        {
+            using (var data = new ContextBase(_optionsBuilder))
+            {
+                await data.Set<T>().AddAsync(obj);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(T obj)
+        {
+            using (var data = new ContextBase(_optionsBuilder))
+            {
+                data.Set<T>().Remove(obj);
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            using (var data = new ContextBase(_optionsBuilder))
+            {
+                return await data.Set<T>().FindAsync(id);
+            }
+        }
+
+        public async Task<List<T>> List()
+        {
+            using (var data = new ContextBase(_optionsBuilder))
+            {
+                return await data.Set<T>().AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task Update(T obj)
+        {
+            using (var data = new ContextBase(_optionsBuilder))
+            {
+                data.Set<T>().Update(obj);
+                await data.SaveChangesAsync();
+            }
+        }
+    }
+}
