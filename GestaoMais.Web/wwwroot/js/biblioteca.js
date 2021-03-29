ee233f888299280b42393fcb97ba925a26b98d46
Cristhyan Kohlhase <cristhyan.ko@outlook.com>
2021-03-28 23:13:36 -0400

    implementando mascara nos campos de cep, telefone, cpf

--- GestaoMais.Web/wwwroot/js/biblioteca.js
+++ GestaoMais.Web/wwwroot/js/biblioteca.js
@@ -1,3 +1,4 @@
-﻿$(document).ready(function () {
-    $('.js-example-basic-single').select2();
-});
+﻿$("#ddd").mask("(00)");
+$("#telefone").mask("0000-00000");
+$("#cpfCnpj").mask("000.000.000-00");
+$("#cep").mask("00000-000");
